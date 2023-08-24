e6750d28fa819991a216eb38f9851cebc28bcffa
Emmanuel Ochade <eochade16@gmail.com>
2023-08-24 16:53:50 +0100

    updated scripts

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const app = express();
 const productRouter = require("./routes/productRoutes");
-const port = process.env.PROT || 3000;
+
 
 dotenv.config();
+
+const port = process.env.PORT || 4000;
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("YEAH!!! DB CONNECTED!!!"))
@@ -14,6 +17,7 @@ mongoose
 // app.get("/", (req, res) => res.send("Hello World!"));
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(cors());
 
 app.use("/api/v1/products", productRouter);
 
