bc787779420a1d41335455daf7699f0b3e7f7fba
Emmanuel Ochade <eochade16@gmail.com>
2023-08-24 18:43:03 +0100

    added description

--- models/products.js
+++ models/products.js
@@ -6,6 +6,10 @@ const ProductSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    description: {
+      type: String,
+      required: true,
+    },
     price: {
       type: String,
       required: true,
